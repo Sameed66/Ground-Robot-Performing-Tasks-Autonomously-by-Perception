a7a16d6da9e27574450c2e9bcdf062f8ddf85913
Sameed66 <82569750+Sameed66@users.noreply.github.com>
2025-11-24 09:17:40 +0100

    Updating

---
+++ ros2/src/turtlebot3_autorace/build/turtlebot3_autorace_camera/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/masters/ros2/src/turtlebot3_autorace/install/turtlebot3_autorace_camera'
